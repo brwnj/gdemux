9f2320ee90e90ee5e812e475117a8be891531d1c
Joe Brown <joe.brown@pnnl.gov>
2016-11-03 15:55:17 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='gdemux',
-    version='0.0.0',
+    version='0.0.1',
     url='http://github.com/brwnj/gdemux',
     license='Unlicense',
     author='Joe Brown',
